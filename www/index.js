462be1ef5bc579a59fc4c3848896d2736675ef5c
Sami Tabet <salph.tabet@gmail.com>
2024-05-05 15:28:38 +0200

    fx

--- www/index.js
+++ www/index.js
@@ -7,53 +7,55 @@ const runner = document.getElementById("btn-runcode");
 const keywords = new Set(wasm.get_keywords());
 
 function recurseText(element, result) {
-    if (element.nodeName === "#text") {
-        element.textContent.split(" ").filter(w => w.length > 0).map(word => {
+  if (element.nodeName === "#text") {
+    element.textContent
+      .split(" ")
+     .filter((w) => w.length > 0)
+      .map((word) => {
         if (keywords.has(word)) {
-            return `<b>${word}</b>`;
+          return `<b>${word}</b>`;
         } else {
-            return word;
+          return word;
         }
-        }).forEach(el => result.push(el));
-    }
+      })
+      .forEach((el) => result.push(el));
+  }
 
-    if (element.childNodes) {
-        element.childNodes.forEach(child => {
-           recurseText(child, result);
-    if (child.nodeName === "DIV") {
-    result.push("\n"); 
-    }
-
-        });
-    }
+  if (element.childNodes) {
+    element.childNodes.forEach((child) => {
+      recurseText(child, result);
+      if (child.nodeName === "DIV") {
+        result.push("\n");
+      }
+    });
+  }
 }
 
 // Very ugly but quick and dirty and avoid stuff such as treesitter
 function syntaxColor(event) {
-    // TODO: color syntax
-    // console.log(event);
-    // const result = [];
-    // recurseText(editor, result);
-    // console.log("collected", result);
-    // console.log(result.join(" "));
-    // editor.innerHTML = result.join(" ");
-
-// var range = document.createRange();
-    //     var sel = window.getSelection();
-    // const selection = window.getSelection();
-    // range.setStart(editor, editor.childNodes.length);
-    // range.collapse(true);
-    // selection.removeAllRanges();
-    // selection.addRange(range);
+  // TODO: color syntax
+  // console.log(event);
+  // const result = [];
+  // recurseText(editor, result);
+  // console.log("collected", result);
+  // console.log(result.join(" "));
+  // editor.innerHTML = result.join(" ");
+  // var range = document.createRange();
+  //     var sel = window.getSelection();
+  // const selection = window.getSelection();
+  // range.setStart(editor, editor.childNodes.length);
+  // range.collapse(true);
+  // selection.removeAllRanges();
+  // selection.addRange(range);
 }
 
 function runcode() {
-    console.log("running code",editor.value );
-   output.textContent=wasm.run(editor.value );
+  console.log("running code", editor.value);
+  output.textContent = wasm.run(editor.value);
 }
 
 editor.addEventListener("input", syntaxColor);
-runner.addEventListener("click",runcode);
+runner.addEventListener("click", runcode);
 
 console.log(wasm.run("print 5+3;"));
 console.log("test", keywords);
